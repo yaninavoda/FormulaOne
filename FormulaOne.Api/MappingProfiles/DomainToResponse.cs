692357357020efb0cce5521393d2b944028b93f8
Ianina Voda <yaninavoda@gmail.com>
2024-03-12 16:52:54 +0200

    add DomainToResponse mapping

--- FormulaOne.Api/MappingProfiles/DomainToResponse.cs
+++ FormulaOne.Api/MappingProfiles/DomainToResponse.cs
@@ -1,6 +1,15 @@
-﻿namespace FormulaOne.Api.MappingProfiles
+﻿using AutoMapper;
+using FormulaOne.Entities.DbSet;
+using FormulaOne.Entities.Dtos.Responses;
+
+namespace FormulaOne.Api.MappingProfiles
 {
-    public class DomainToResponse
+    public class DomainToResponse : Profile
     {
+        public DomainToResponse()
+        {
+            CreateMap<Achievement, DriverAchievementResponse>()
+                .ForMember(dest => dest.Wins, options => options.MapFrom(src => src.RaceWins));
+        }
     }
 }
