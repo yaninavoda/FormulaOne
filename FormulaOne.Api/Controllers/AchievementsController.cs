f4a357b540ef70d9d2ac54c322cf68dc94d7f7c8
Ianina Voda <yaninavoda@gmail.com>
2024-03-12 17:07:53 +0200

    add Update and Create to AchievementsController

--- FormulaOne.Api/Controllers/AchievementsController.cs
+++ FormulaOne.Api/Controllers/AchievementsController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using FormulaOne.DataService.Repositories.Interfaces;
+using FormulaOne.Entities.DbSet;
+using FormulaOne.Entities.Dtos.Requests;
 using FormulaOne.Entities.Dtos.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +23,41 @@ namespace FormulaOne.Api.Controllers
             var result = _mapper.Map<DriverAchievementResponse>(driverAchievements);
             
             return Ok(result);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddAchievement([FromBody]CreateDriverAchievementRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = _mapper.Map<Achievement>(request);
+
+            await _unitOfWork.Achievements.Add(result);
+            await _unitOfWork.CompleteAsync();
+
+            return CreatedAtAction(
+                nameof(GetDriverAchievements),
+                new { driverId = result.DriverId },
+                result);
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateAchievement([FromBody] CreateDriverAchievementRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = _mapper.Map<Achievement>(request);
+
+            await _unitOfWork.Achievements.Update(result);
+            await _unitOfWork.CompleteAsync();
+
+            return NoContent();
         }
     }
 }
